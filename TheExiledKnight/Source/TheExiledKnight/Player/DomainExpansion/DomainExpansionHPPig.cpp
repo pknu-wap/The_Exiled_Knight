4c2b8a32f1c42ce65443bb53ed610809146e1641
KaneBigNose <imddongs@naver.com>
2024-11-26 18:49:53 +0900

    Feat: HPPig 영역 전개 추가

--- TheExiledKnight/Source/TheExiledKnight/Player/DomainExpansion/DomainExpansionHPPig.cpp
+++ TheExiledKnight/Source/TheExiledKnight/Player/DomainExpansion/DomainExpansionHPPig.cpp
@@ -6,26 +6,19 @@ ADomainExpansionHPPig::ADomainExpansionHPPig()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
-	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ADomainExpansionHPPig::OnOverlapBegin);
 	SphereComponent->OnComponentEndOverlap.AddDynamic(this, &ADomainExpansionHPPig::OnOverlapEnd);
 }
 
 void ADomainExpansionHPPig::BeginPlay()
 {
 	Super::BeginPlay();
+
+	EKPlayer->EKPlayerStateContainer.AddTag(EKPlayerGameplayTags::EKPlayer_State_DomainExpansion_HPPig);
 }
 
 void ADomainExpansionHPPig::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-}
-
-void ADomainExpansionHPPig::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	if (OverlappedComponent->IsA(AEKPlayer::StaticClass()))
-	{
-		EKPlayer->EKPlayerStateContainer.AddTag(EKPlayerGameplayTags::EKPlayer_State_DomainExpansion_HPPig);
-	}
 }
 
 void ADomainExpansionHPPig::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
